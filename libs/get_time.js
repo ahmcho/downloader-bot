3197fb2b6109f7d8486beda0a580c02f1a17e6fc
ahmcho <mail@ahmcho.com>
2023-07-03 01:33:39 +0400

    refactor app.js

--- libs/get_time.js
+++
@@ -1,5 +0,0 @@
-const getCurrentTime = () => {
-    return new Date(((new Date()).setHours((new Date().getHours() + 4)))).toISOString().split('T');
-}
-
-module.exports.getCurrentTime = getCurrentTime
--- libs/list_builder.js
+++
@@ -1,9 +0,0 @@
-const buildPaginatedSiteList = (arrayOfWebsites) => {
-    let finalMessage = '';
-    arrayOfWebsites.map((website) => {
-        finalMessage += `• <code>${website}</code>\n`;
-    });
-    return finalMessage;
-}
-
-module.exports.buildPaginatedSiteList = buildPaginatedSiteList;
