9037251125452fe672d290afd55ff5564716eea3
ahmcho <mail@ahmcho.com>
2023-07-08 16:54:56 +0400

    add middleware to prevent other users accessing your bot

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
 require('dotenv').config();
+
 const {
     Telegraf,
 } = require('telegraf');
 
 const Downloader = require('./src/downloader');
 
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const downloader = new Downloader();
+
+bot.use(async (ctx, next) => {
+    // Failsafe to prevent others from using this bot, otherwise everyone will be send links for downloading to YOUR computer.
+    if (
+        ctx.update.message.from.id == process.env.ME 
+        || 
+        ctx.update.message.chat.id == process.env.ME
+    )
+    {
+        await next();
+    } else {
+        await ctx.reply('Unauthorized');
+    }
+})
 
 bot.on('document', async (ctx) => {
     downloader.handleDocument(ctx);
 });
 
 bot.hears(/mp3 (.+)/, async (ctx) => {
-    if (ctx.update.message.from.id == process.env.ME || ctx.update.message.chat.id == process.env.ME) {
-        //  ⬆️ Failsafe to prevent others from using this bot, otherwise everyone will be send links for downloading to YOUR computer.
-        downloader.handleAudio(ctx);
-    } else {
-        ctx.reply('Unauthorized');
-    }
+    downloader.handleAudio(ctx);
 });
 
 bot.hears('Check supported websites 🌍', async (ctx) => {
-    if (ctx.update.message.from.id == process.env.ME || ctx.update.message.chat.id == process.env.ME) {
-        downloader.handleSupportedWebsites(ctx);
-    }
+    downloader.handleSupportedWebsites(ctx);
 });
 
 bot.action(/.+/, async (ctx) => {
@@ -38,17 +45,11 @@ bot.hears('F.A.Q ℹ️', async (ctx) => {
 });
 
 bot.hears('Check for the updates 🔄', async (ctx) => {
-    if (ctx.update.message.from.id == process.env.ME || ctx.update.message.chat.id == process.env.ME) {
-        downloader.handleUpdate(ctx);
-    }
+    downloader.handleUpdate(ctx);
 });
 
 bot.hears(/video (.+)/, async (ctx) => {
-    if (ctx.update.message.from.id == process.env.ME || ctx.update.message.chat.id == process.env.ME) {
-        downloader.handleVideo(ctx);
-    } else {
-        await ctx.reply('Unauthorized');
-    }
+    downloader.handleVideo(ctx);
 });
 
 bot.help(async (ctx) => {
